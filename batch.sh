d44cdfa6bbc7348e832abb6dce7eba21aa1e1937
gmlyytt-YANG <gmlyytt@outlook.com>
2018-11-28 22:33:55 +0800

    rm bash.sh

--- batch.sh
+++
@@ -1,9 +0,0 @@
-sh run.sh cmd 100 32 1e-3 &&
-sh run.sh cmd 100 64 1e-3 &&
-sh run.sh cmd 100 128 1e-3 &&
-sh run.sh cmd 100 32 1e-4 &&
-sh run.sh cmd 100 64 1e-4 &&
-sh run.sh cmd 100 128 1e-4 &&
-sh run.sh cmd 100 32 1e-3 &&
-sh run.sh cmd 100 64 1e-3 &&
-sh run.sh cmd 100 128 1e-3 
